aaa8e472352dc4e6921fa1bc9db2dfb24ee2c977
Holly919 <56270260+Holly919@users.noreply.github.com>
2020-09-21 13:57:02 -0400

    router and db created

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const carsRouter = require('../cars/cars-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+server.get('/', (req, res, next) => {
+    res.status(200).json({api: "Vroom, vroom bitches"})
+  })
+
+module.exports = server;
